b766e6ec81f5f2a298c51501fc6aad51ae295998
slpi1 <1498518288@qq.com>
2015-07-30 14:08:17 +0800

    move user site method from Profile to Site
    Signed-off-by: slpi1 <1498518288@qq.com>

--- Application/Admin/Controller/ProfileController.class.php
+++ Application/Admin/Controller/ProfileController.class.php
@@ -15,44 +15,33 @@ class ProfileController extends AdminController {
     }
 
     /**
-     * 编辑站点基本信息
+     * 管理我的站点
      */
     public function site(){
-        $uid        =  UID;
-        $list       =   M("Site")->where("manage=$uid")->order('id asc')->select();
-        int_to_string($list,array('status'=>array(0=>"关闭",1=>"开启")));
-    
-        $this->assign('list',$list);
-        // 记录当前列表页的cookie
-        Cookie('__forward__',$_SERVER['REQUEST_URI']);
+        $a = I('get.smethod',"mysite");
 
-        $this->meta_title = '我的站点';
-        $this->display();
-    }
+        $Site = new SiteController();
+        $res = $Site->$a();
 
-    public function editsite(){
-        if(IS_POST){
-            $Site = D('Site');
-            $data = $Site->create();
-            if($data){
-                if($Site->save()!== false){
-                    $this->success('更新成功', Cookie('__forward__'));
-                } else {
-                    $this->error('更新失败');
-                }
-            } else {
-                $this->error($Site->getError());
+        $return = array('editsite','category');
+        if( in_array($a,$return) ){
+            
+            if( $res == 1 ){
+                $info = array(
+                    'status'    => 1,
+                    'info'      => "操作成功"
+                );
+            }else{
+                $info=array(
+                    'status'    => ($res == 1) ? 1 : 0,
+                    'info'      => $Site->profile_info[$res],
+                );
             }
-        } else {
-            $info = array();
-            /* 获取数据 */
-            $info = M('Site')->field(true)->find($id);
-            $this->assign('info', $info);
-            $this->assign('manages',$this->manages);
-            $this->meta_title = '编辑站点';
-            $this->display();
+
+            $this->ajaxReturn($info);
         }
     }
+
 
     /**
      * 设置站点状态
@@ -75,47 +64,5 @@ class ProfileController extends AdminController {
         }
     }
 
-    public function manage(){
-        $id = I('get.id');
-
-        if(empty($id)){
-            $this->error("请选择要管理的站点！");
-        }
-
-        $site_info = M("Site")->find($id);
-        if( !$site_info ){
-            $this->error("站点不存在！");
-        }
-        if( $site_info['manage'] != UID ){
-            $this->error("您无权管理他人站点！");
-        }
-
-
-        $cate_list     =   D('Category')->getTree();
-        $this->assign('cate_list',     $cate_list);
-
-        $this->assign("info",$site_info);
-        // 记录当前列表页的cookie
-        Cookie('__forward__',$_SERVER['REQUEST_URI']);
-        $this->meta_title = '管理站点';
-        $this->display();
-    }
-
-    public function category(){
-        $cid = I("cid");
-        $site_id = I("site_id");
-
-        if( empty($site_id) ){
-            $this->error("参数错误，没有指定要操作的站点");
-        }
-
-        $cid = implode(',',$cid);
-
-        $res = M("Site")->where("id = $site_id ")->setField('category',$cid);
-
-        if( $res ){
-            $this->success("添加分类成功！");
-        }
-    }
 
 }
--- Application/Admin/Controller/SiteController.class.php
+++ Application/Admin/Controller/SiteController.class.php
@@ -8,6 +8,15 @@ class SiteController extends AdminController {
 
     private $manages = array();
 
+    public $profile_info = array(
+        '-2' =>"数据保存失败",
+        '-1' =>"没有选择要操作站点",
+        '0' =>"所选栏目没有改变",
+    );
+
+    /**
+     * 初始化
+     */
     public function _initialize(){
         parent::_initialize();
 
@@ -15,11 +24,21 @@ class SiteController extends AdminController {
         $this->manages = M("Member")->getField("uid,nickname");
     }
 
+    /**
+     * 站点列表
+     */
     public function index(){
         $manages = $this->manages;
 
         $list       =   M("Site")->order('id asc')->select();
-        int_to_string($list,array('status'=>array(0=>"关闭",1=>"开启"),'manage'=>$manages));
+        int_to_string($list,array(
+                'status'=>array(
+                    0=>"<span class='badge badge-danger'>关闭</span>",
+                    1=>"<span class='badge badge-success'>开启</span>"
+                ),
+                'manage'=>$manages
+            )
+        );
     
         $this->assign('list',$list);
         // 记录当前列表页的cookie
@@ -28,6 +47,7 @@ class SiteController extends AdminController {
         $this->meta_title = '站点列表';
         $this->display();
     }
+
 
     /**
      * 新增站点
@@ -53,6 +73,10 @@ class SiteController extends AdminController {
         }
     }
 
+    /**
+     * 编辑站点
+     * @param  integer $id 站点id
+     */
     public function edit($id = 0){
         if(IS_POST){
             $Site = D('Site');
@@ -80,7 +104,8 @@ class SiteController extends AdminController {
     /**
      * 设置站点状态
      */
-    public function set($status){
+    public function set(){
+        $status = I('status');
         $id = array_unique((array)I('id',0));
 
         if ( empty($id) ) {
@@ -100,7 +125,6 @@ class SiteController extends AdminController {
 
     /**
      * 删除站点
-     * @author yangweijie <yangweijiester@gmail.com>
      */
     public function del(){
         $id = array_unique((array)I('id',0));
@@ -117,6 +141,9 @@ class SiteController extends AdminController {
         }
     }
 
+    /**
+     * 批量导入数据
+     */
     public function import(){
         if(IS_POST){
             $tree = I('post.tree');
@@ -153,5 +180,94 @@ class SiteController extends AdminController {
             $this->display();
         }
     }
+
+
+    /**************************
+     * Profile site method    *  
+     **************************/
+    /**
+     * 我的站点列表
+     */
+    public function mysite(){
+        $uid        =  UID;
+        $list       =   M("Site")->where("manage=$uid")->order('id asc')->select();
+        int_to_string($list,array(
+                'status'=>array(
+                    0=>"<span class='badge badge-danger'>关闭</span>",
+                    1=>"<span class='badge badge-success'>开启</span>"
+                )
+            )
+        );
+    
+        $this->assign('list',$list);
+        // 记录当前列表页的cookie
+        Cookie('__forward__',$_SERVER['REQUEST_URI']);
+
+        $this->meta_title = '我的站点';
+        $this->display('Site/Profile/mysite');
+    }
+    
+    /**
+     * 站点内容管理
+     */
+    public function manage(){
+        $id = I('get.id');
+
+        if(empty($id)){
+            $this->error("请选择要管理的站点！");
+        }
+
+        $status = array(0=>'关闭',1=>'开启');
+        $site_info = M("Site")->find($id);
+        $site_info['status_text'] = $status[$site_info['status']];
+        if( !$site_info ){
+            $this->error("站点不存在！");
+        }
+        if( $site_info['manage'] != UID ){
+            $this->error("您无权管理他人站点！");
+        }
+
+
+        $cate_list     =   D('Category')->getTree();
+        $this->assign('cate_list',     $cate_list);
+
+        $this->assign("info",$site_info);
+        // 记录当前列表页的cookie
+        Cookie('__forward__',$_SERVER['REQUEST_URI']);
+        $this->meta_title = '管理站点';
+        $this->display('Site/Profile/manage');
+    }
+
+    /**
+     * 基础信息管理
+     */
+    public function editsite(){
+        $Site = D('Site');
+        $data = $Site->create();
+        if($data){
+            return $Site->save();
+        } else {
+            return -2;
+        }
+    }
+
+    /**
+     * 给站点添加分类
+     */
+    public function category(){
+        $cid = I("cid");
+        $site_id = I("site_id");
+
+        if( empty($site_id) ){
+            return -1;
+        }
+
+        $cid = implode(',',$cid);
+
+        $res = M("Site")->where("id = $site_id ")->setField('category',$cid);
+
+        return $res;
+    }
+
 
 }
