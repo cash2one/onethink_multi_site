ab3897316c95d1a9128b1f2bc0fa1dd8045bed38
slpi1 <1498518288@qq.com>
2015-12-23 11:05:39 +0800

    新增api模型
    Signed-off-by: slpi1 <1498518288@qq.com>

---
+++ Application/Home/Logic/ChiralLogic.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Home\Logic;
+
+/**
+ * 文档模型子模型 - 文章模型
+ */
+class ChiralLogic extends BaseLogic{
+	/* 自动验证规则 */
+	protected $_validate = array();
+
+	/* 自动完成规则 */
+	protected $_auto = array();
+
+}
